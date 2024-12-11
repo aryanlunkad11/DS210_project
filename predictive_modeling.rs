73171dc6d9fd83309706a723c2b1ff4bd207618f
aryanlunkad11 <135801422+aryanlunkad11@users.noreply.github.com>
2024-12-11 16:55:34 -0500

    Delete predictive_modeling.rs

--- predictive_modeling.rs
+++
@@ -1,7 +0,0 @@
-use crate::data_processing::Property;
-
-pub fn build_predictive_model(data: &[Property]) -> Vec<f64> {
-    data.iter()
-        .map(|p| p.rent_per_sqft.unwrap_or(0.0) * 1.1) // Example prediction
-        .collect()
-}
