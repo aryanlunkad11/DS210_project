2bbb749277a5c218db28759759d80c74a067cd02
aryanlunkad11 <135801422+aryanlunkad11@users.noreply.github.com>
2024-12-15 20:32:27 -0500

    Delete src directory

--- src/predictive_modeling.rs
+++
@@ -1,7 +0,0 @@
-use crate::Property;
-
-pub fn build_predictive_model(data: &[Property]) -> Vec<f64> {
-    data.iter()
-        .map(|p| p.rent_per_sqft.unwrap_or(0.0) * 1.1)
-        .collect()
-}
